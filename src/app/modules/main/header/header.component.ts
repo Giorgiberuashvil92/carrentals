13d9810222625032dfe0d633ee56ccbf4d187e29
Gberuashvili <beruashviligio7@gmail.com>
2020-10-19 08:26:54 +0400

    fix

--- src/app/modules/main/header/header.component.ts
+++ src/app/modules/main/header/header.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  imageSrc = '../../../assetsImg/images/user.png'
+  imageAlt = 'User'
+
+  constructor() {}
 
   ngOnInit(): void {
   }
+    navigation = [
+    {url: 'www.google.ge', name:'Plan Another Trip'},
+    {url: 'www.google.ge', name:'All My Trips'},
+    {url: 'www.google.ge', name:'Before Trips'},
+    {url: 'www.google.ge', name:'FAQ'},
+    {url: 'www.google.ge', name:'Contact Us'},
+  ];
 
 }
