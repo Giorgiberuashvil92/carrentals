12c80287a55646b9cfbcd53136d5685678d59193
Gberuashvili <beruashviligio7@gmail.com>
2020-10-20 01:13:10 +0400

    fix

--- src/app/modules/main/footer/footer.component.ts
+++ src/app/modules/main/footer/footer.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+     array = [
+    {name:'Traveler References'},
+    {name: 'Terms of Use'},
+    {name: 'FAQ'}
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
