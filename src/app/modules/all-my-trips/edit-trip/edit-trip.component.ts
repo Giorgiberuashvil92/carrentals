112438f0f5ca3671a95dc1d7c8e571b0268f6c56
Giorgi Beruashvili <g.beruashvili@adjarabet.com>
2020-10-23 20:29:10 +0400

    fix

--- src/app/modules/all-my-trips/edit-trip/edit-trip.component.ts
+++ src/app/modules/all-my-trips/edit-trip/edit-trip.component.ts
@@ -34,7 +34,7 @@ export class EditTripComponent implements OnInit {
 
   currentIndex = -1;
   newIndex = 0;
-
+  currentlyChosenIndex = -1;
   citySet = new Set<string>();
   interestSet = new Set<string>();
   acitivityInput: string = '';
--- src/app/modules/main/header/desktop/header.component.ts
+++ src/app/modules/main/header/desktop/header.component.ts
@@ -9,11 +9,17 @@ export class HeaderComponent implements OnInit {
 
   imageSrc = '../../../assetsImg/images/user.png'
   imageAlt = 'User'
+  condition: boolean;
 
-  constructor() {}
+  constructor() {
+    
+  }
 
   ngOnInit(): void {
   }
+   
+
+
     navigation = [
     {url: 'www.google.ge', name:'Plan Another Trip'},
     {url: 'www.google.ge', name:'All My Trips'},
@@ -21,5 +27,6 @@ export class HeaderComponent implements OnInit {
     {url: 'www.google.ge', name:'FAQ'},
     {url: 'www.google.ge', name:'Contact Us'},
   ];
+
 
 }
