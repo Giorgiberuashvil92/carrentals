fd89ea884f2eb100b6ca6c0b6dfc6c1b916bc3f7
Giorgi Beruashvili <g.beruashvili@adjarabet.com>
2020-11-02 18:56:34 +0400

    fix

---
+++ src/app/store/effects/session.effects.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Actions, Effect, ofType } from '@ngrx/effects';
+import { map, mergeMap, catchError } from 'rxjs/operators';
+
+import { of } from 'rxjs';
+import { UserService } from 'src/app/core/services/user.service';
+import { PutProfileAction, SessionAction } from '../actions';
+import { LoadSessionFailureAction, LoadSessionSuccessAction, SessionActionTypes } from '../actions/session.action';
+
+@Injectable()
+export class SessionEffects {
+    [x: string]: any;
+
+    constructor(
+      private actions$: Actions,
+      private profileService: UserService
+    ) { }
+
+    @Effect() loadProfile$ = this.actions$
+    .pipe(
+        ofType<SessionAction>(SessionActionTypes.LOAD_SESSION),
+        mergeMap(
+        (d) => this.userService.getSession()
+            .pipe(
+            map(data => {
+                return new LoadSessionSuccessAction(data)
+            }),
+            catchError(error => of(new LoadSessionFailureAction(error)))
+            )
+        ),
+    )
+
+}
